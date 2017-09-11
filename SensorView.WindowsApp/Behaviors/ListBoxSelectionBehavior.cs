0dc4b449e3f1268a2501fe8d25c13898c1dfb6b9
usausa <machi.pon@gmail.com>
2017-09-11 17:14:13 +0900

    Add ListBoxSelectionBehavior

---
+++ SensorView.WindowsApp/Behaviors/ListBoxSelectionBehavior.cs
@@ -0,0 +1,66 @@
+﻿namespace SensorView.WindowsApp.Behaviors
+{
+    using System.Collections;
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class ListBoxSelectionBehavior : Behavior<ListBox>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (AssociatedObject == null)
+            {
+                return;
+            }
+
+            AssociatedObject.SelectionMode = SelectionMode.Multiple;
+            AssociatedObject.SelectionChanged += OnSelectionChanged;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            if (AssociatedObject != null)
+            {
+                AssociatedObject.SelectionChanged -= OnSelectionChanged;
+            }
+
+            base.OnDetaching();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 0)
+            {
+                return;
+            }
+
+            if (sender is ListBox listBox)
+            {
+                var target = e.AddedItems[0];
+                foreach (var item in new ArrayList(listBox.SelectedItems))
+                {
+                    if (item != target)
+                    {
+                        listBox.SelectedItems.Remove(item);
+                    }
+                }
+            }
+        }
+    }
+}
